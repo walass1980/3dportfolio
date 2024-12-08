a3112c42f45cf4132a789d247f927224c69d0ca9
walass <cha_wal100@yahoo.fr>
2024-12-08 20:34:22 +0100

    up

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,16 @@
 import "./hero.css"
 const Hero = () => {
   return (
-    <div className=''>Hero</div>
+    <div className='hero'>
+      <div className="hSection left">
+        <h1 className="hTitle">
+          Hey There,
+          <br />
+          <span>I'm Walid!</span>
+        </h1>
+      </div>
+      <div className="hSection right"></div>
+    </div>
   )
 }
 
