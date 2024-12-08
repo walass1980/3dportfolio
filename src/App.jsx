c75543c16a7bbf528008b2b802b0b462926afdbd
walass <cha_wal100@yahoo.fr>
2024-12-08 17:56:08 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,18 @@ import Services from "./components/services/Services"
 const App = () => {
   return (
     <div className='container'>
-      <Hero/>
-      <Services/>
-      <Portfolio/>
-      <Contact/>
+      <section id="#home">
+        <Hero/>
+      </section>
+      <section id="#services">
+        <Services/>
+      </section>
+      <section id="#portfolio">
+        <Portfolio/>
+      </section>
+      <section id="#contact">
+        <Contact/>
+      </section>
     </div>
   )
 }
