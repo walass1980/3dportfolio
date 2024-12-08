599d18ec20d05245a8c824b3250d7e8da0d59f42
walass <cha_wal100@yahoo.fr>
2024-12-08 20:39:55 +0100

    up

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,6 +8,15 @@ const Hero = () => {
           <br />
           <span>I'm Walid!</span>
         </h1>
+        <div className="awards">
+          <h2>Top Rated Designer</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <div className="awardList">
+            <img src="/award1.png" alt="" />
+            <img src="/award2.png" alt="" />
+            <img src="/award3.png" alt="" />
+          </div>
+        </div>
       </div>
       <div className="hSection right"></div>
     </div>
