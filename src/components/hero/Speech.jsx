17bddf59f2bc17b8ed489a851d98af095b9ccff3
walass <cha_wal100@yahoo.fr>
2024-12-10 21:10:11 +0100

    up

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,11 +1,27 @@
-
+import { TypeAnimation } from "react-type-animation"
+import { motion } from "motion/react"
 
 const Speech = () => {
   return (
-    <div className="bubbleContainer">
-        <div className="bubble">Text</div>
+    <motion.div className="bubbleContainer">
+        <div className="bubble">
+        <TypeAnimation
+          sequence={[
+            1000,
+            "Same substring at the start will only be typed out once, initially",
+            1000,
+            "Lorem ipsum dolor sit amet lerinat consectetur adipisicing. ",
+            1000,
+          ]}
+          wrapper="span"
+          speed={40}
+          deletionSpeed={60}
+          // omitDeletionAnimation
+          repeat={Infinity}
+        />
+        </div>
         <img src="/man.png" alt=""/>
-    </div>
+    </motion.div>
   )
 }
 
