0103f7d77c3ff9bf597ed8660e9e5c5561897fcc
walass <cha_wal100@yahoo.fr>
2024-12-10 20:54:01 +0100

    up

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,21 @@
 import "./hero.css"
 import Speech from "./Speech"
+
+const awardVariants = {
+  initial: {
+    x: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
 const Hero = () => {
   return (
     <div className='hero'>
