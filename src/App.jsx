366dc339e6f8a608d8a6e1f0e0b3db1abadf10fc
walass <cha_wal100@yahoo.fr>
2024-12-12 08:30:37 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Portfolio from "./components/portfolio/Portfolio"
 import Services from "./components/services/Services"
 
 const App = () => {
+
   return (
     <div className='container'>
       <section id="#home">
---
+++ src/components/hero/Shap.jsx
@@ -0,0 +1,9 @@
+import { MeshDiscardMaterial, Sphere } from "@react-three/drei"
+
+const Shap = () => {
+  return (
+    <div>Shap</div>
+  )
+}
+
+export default Shap
