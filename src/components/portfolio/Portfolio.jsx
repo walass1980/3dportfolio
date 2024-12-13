599297de3fe50ff84db9db4d0a3aab45e9ec7793
walass <cha_wal100@yahoo.fr>
2024-12-13 22:21:46 +0100

    up

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,44 @@
 import "./portfolio.css"
 
+const items = [
+  {
+    id: 1,
+    img: "/p1.jpg",
+    title: "Full Stack Blog Application",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 2,
+    img: "/p2.jpg",
+    title: "School Management System",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 3,
+    img: "/p3.jpg",
+    title: "Real-time Chat Application",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 4,
+    img: "/p4.jpg",
+    title: "Social Media Project",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 5,
+    img: "/p5.jpg",
+    title: "Animated Portfolio Website",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+];
+
+
 const Portfolio = () => {
   return (
     <div>Portfolio</div>
