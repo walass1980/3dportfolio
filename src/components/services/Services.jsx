d27bb6371faea8d74789ac519eb902204c578771
walass <cha_wal100@yahoo.fr>
2024-12-13 15:11:57 +0100

    up

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -4,9 +4,22 @@ import ComputerModelContainer from "./computer/ComputerModelContainer"
 const Services = () => {
   return (
     <div className="services">
-      <div className="sSection left"></div>
+      <div className="sSection left">
+        <h1 className="sTitle">How do I help us?</h1>
+        <div className="serviceList">
+          <div className="service">
+            <div className="serviceIcon">
+              <img src="" alt="" />
+            </div>
+            <div className="serviceInfo">
+              <h2>Lorem ipsum dolor sit amet consectetur</h2>
+              <h3>3 projects</h3>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="sSection right">
-        <ComputerModelContainer/>
+        
       </div>
     </div>
   )
