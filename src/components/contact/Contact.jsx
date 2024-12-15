de661b611fbb118f4d597bfdc6a5fd218d9accb9
walass <cha_wal100@yahoo.fr>
2024-12-15 19:41:52 +0100

    up

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,24 @@
+
 import "./contact.css"
+
+const listVariant = {
+  initial: {
+    x: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggetChildren: 0.2,
+    }
+  }
+}
 
 const Contact = () => {
   return (
-    <div className="contact">Contact</div>
+    <div className="contact"></div>
   )
 }
 
