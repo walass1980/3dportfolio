248b43b7e78d9967ee99dfdc267cf5707b2030e0
walass <cha_wal100@yahoo.fr>
2024-12-15 19:56:20 +0100

    up

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,6 @@
 
 import "./contact.css"
+import ContactSvg from "./ContactSvg"
 
 const listVariant = {
   initial: {
@@ -17,8 +18,39 @@ const listVariant = {
 }
 
 const Contact = () => {
+  const 
   return (
-    <div className="contact"></div>
+    <div className="contact">
+      <div className="cSection">
+        <form>
+          <h1>Let's keep in touch</h1>
+          <div className="formItem">
+            <label>Name</label>
+            <input type="text" name="user_name" placeholder="John Doe" />
+          </div>
+          <div className="formItem">
+            <label>Email</label>
+            <input 
+             type="email" 
+             name="user_name"
+             placeholder="john@gmail.com"
+            />
+          </div>
+          <div className="formItem">
+            <label>Message</label>
+            <textarea 
+             rows={10}
+             name="user_message"
+             placeholder="Write your message..."
+            ></textarea>
+          </div>
+          <button className="formButton">
+            Send
+          </button>
+        </form>
+      </div>
+      <div className="cSection"><ContactSvg/></div>
+    </div>
   )
 }
 
